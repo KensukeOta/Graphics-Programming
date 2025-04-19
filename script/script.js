3e13ecf29483f20b21030893c9480e54eb7c0c28
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2025-04-19 09:48:02 +0900

    メインループを実装

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * canvasの幅
  * @type {number}
@@ -28,6 +30,11 @@ let ctx = null;
  * @type {Image}
  */
 let image = null;
+/**
+ * 実行開始時のタイムスタンプ
+ * @type {number}
+ */
+let startTime = null;
 
 window.addEventListener("load", () => {
   // ユーティリティクラスを初期化
@@ -43,6 +50,8 @@ window.addEventListener("load", () => {
     image = loadedImage;
     // 初期化処理を行う
     initialize();
+    // 実行開始時のタイムスタンプを取得する
+    startTime = Date.now();
     // 描画処理を行う
     render();
   });
@@ -57,6 +66,18 @@ function initialize() {
 function render() {
   // 描画前に画面全体を不透明な明るいグレーで塗りつぶす
   util.drawRect(0, 0, canvas.width, canvas.height, "#eeeeee");
-  // 画像を描画する
-  ctx.drawImage(image, 100, 100);
+
+  // 現在までの経過時間を取得する（ミリ秒を秒に変換するため1000で除算）
+  let nowTime = (Date.now() - startTime) / 1000;
+  // 時間の経過が見た目にわかりやすいように自機をサイン波で動かす
+  let s = Math.sin(nowTime);
+  // サインやコサインは半径1の円を基準にしているので、得られる値の範囲が
+  // -1.0〜1.0になるため、効果が分かりやすくなるように100倍する
+  let x = s * 100.0;
+  
+  // 画像を描画する（canvasの中心位置を基準にサイン波で左右に往復するようにする）
+  ctx.drawImage(image, CANVAS_WIDTH / 2 + x, CANVAS_HEIGHT / 2);
+
+  // 恒常ループのために描画処理を再帰呼び出しする
+  requestAnimationFrame(render);
 }
